25226a061deddc9ee3e3afbee21fecddf1c6a706
Moisés J. Bonilla Caraballo <moisesjbc@gmail.com>
2018-12-02 18:03:27 +0000

    Added option for toggling sound mode

--- ludum-dare-43/menus/main_menu/main_menu.gd
+++ ludum-dare-43/menus/main_menu/main_menu.gd
@@ -1,5 +1,12 @@
 extends CanvasLayer
 
+
+
+
+func _ready():
+	print_sound_mode()
+
+
 func _on_start_game_button_pressed():
 	get_tree().change_scene("res://main/main.tscn")
 
@@ -10,3 +17,21 @@ func _on_exit_game_button_pressed():
 
 func _on_credits_button_pressed():
 	get_tree().change_scene("res://menus/credits_menu/credits_menu.tscn")
+
+
+func _on_toggle_sound_button_pressed():
+	get_node('/root/global').toggle_sound_mode()
+	print_sound_mode()
+
+
+func print_sound_mode():
+	var sound_mode = get_node('/root/global').sound_mode
+	var sound_mode_label = ''
+	if sound_mode == get_node('/root/global').SoundMode.FULL:
+		sound_mode_label = 'FULL'
+	elif sound_mode == get_node('/root/global').SoundMode.FX_ONLY:
+		sound_mode_label = 'FX ONLY'
+	else:
+		sound_mode_label = 'MUTED'
+		
+	$CenterContainer/VBoxContainer/toggle_sound_button.text = "Toggle sound mode [" + sound_mode_label + "]"
